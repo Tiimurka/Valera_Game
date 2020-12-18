a4ac0c93d4705d8b45962cd385b876e49fcaeeb9
Tiimurka <timur22211@gmail.com>
2020-12-18 15:59:22 +0700

    First version of Valera class

--- lib/yaml_test.rb
+++ lib/yaml_test.rb
@@ -1,4 +1,5 @@
 require 'yaml'
+require_relative 'valera'
 
 yaml_test = YAML.load_file('action_config.yml')
 /puts yaml_test['fun']/
@@ -8,3 +9,13 @@ while i < yaml_test.count do
     puts yaml_test[yaml_test.keys[i]]
 	i+=1
 end
+
+v = Valera.new
+puts
+i = 0
+while i < yaml_test.count do
+    puts v.stats[v.stats.keys[i]]
+	i+=1
+end
+
+puts v.is_death
